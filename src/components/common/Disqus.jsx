c9e94a5ee860b45207ecb29ee0b08222d32565a0
SDK <admin@sundo.kim>
2023-01-13 00:10:13 +0900

    왜 안되는 것이냐?

--- src/components/common/Disqus.jsx
+++ src/components/common/Disqus.jsx
@@ -9,15 +9,13 @@ interface DisqusProps {
     title: string;
 }
 
-const Disqus: React.FC<DisqusProps> = ({ identifier, title }) => {
-    identifier = window.location.pathname + "_" + identifier + "_disqus";
-    identifier = identifier.split('-').join('_').trim().replace('/','');
+const Disqus: React.FC<DisqusProps> = ({ identifier, title}) => {
+    const env = window.location.href.includes('localhost') ? 'dev' : 'prod'
+    identifier = window.location.pathname.replace('/','') + "_" + identifier + `_${env}`;
     console.log(identifier)
     const [config, setConfig] = React.useState({
-        url: "https://cbt.sdk.xyz/",
-        // url: window.location.href,
+        url: window.location.href,
         identifier : identifier,
-        // identifier : window.location.href + "_" + identifier.split('-').join('_'),
         title,
         language: 'ko',
     });
