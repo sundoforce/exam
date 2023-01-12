8914d8a37785b6b95fbb09a59c72a580e086af72
SDK <admin@sundo.kim>
2023-01-12 10:32:43 +0900

    댓글 TEST : 실제 real question number와 댓글이 잘 매칭되는지 확인하기 위한 page log 출력

--- src/components/quizzes/ExamList.js
+++ src/components/quizzes/ExamList.js
@@ -43,6 +43,7 @@ function Quiz() {
                 return (
                     <div key={question.id}>
                         <h3>{page}. {question.title}</h3>
+                        <p>real question number: {question.id}</p>
                         {question.passage_a && <p><input type="radio" name={question.id} value="A" checked={answers[question.id] === 'A'} onChange={handleChange} /> A. {question.passage_a}</p>}
                         {question.passage_b && <p><input type="radio" name={question.id} value="B" checked={answers[question.id] === 'B'} onChange={handleChange} /> B. {question.passage_b}</p>}
                         {question.passage_c && <p><input type="radio" name={question.id} value="C" checked={answers[question.id] === 'C'} onChange={handleChange} /> C. {question.passage_c}</p>}
