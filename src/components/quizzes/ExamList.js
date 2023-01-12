9095ad44f1f900970b1ed7300564fc061b5e4596
SDK <admin@sundo.kim>
2023-01-13 01:13:24 +0900

    refactor

--- src/components/quizzes/ExamList.js
+++ src/components/quizzes/ExamList.js
@@ -8,14 +8,20 @@ const Quiz = (props) => {
     const [page, setPage] = useState(1);
     const [start, SetStart] = useState(props.state?.start || 1);
     const [end, SetEnd] = useState(props.end?.end || 200);
-
+    const [showAnswer, setShowAnswer] = useState({});
+    const [isQuizFinished, setIsQuizFinished] = useState(false);
 
     // Use the useEffect hook to retrieve the quiz data from the API when the component mounts
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`https://cbt-api.sdk.xyz/api/quiz/randoms/${start}/${end}`);
-            const data = await response.json();
-            setQuizData(data);
+            try {
+                const response = await fetch(`https://cbt-api.sdk.xyz/api/quiz/randoms/${start}/${end}`);
+                const data = await response.json();
+                setQuizData(data);
+            } catch (err) {
+                console.log(err);
+                // handle error
+            }
         }
 
         fetchData();
@@ -38,11 +44,24 @@ const Quiz = (props) => {
     function handleNext() {
         setPage((prevPage) => prevPage + 1);
     }
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setAnswers((prevAnswers) => ({...prevAnswers, [name]: value}));
+    }
+
+    // Function to handle the "Show Answer" button click
+    function handleShowAnswer(questionId, answer) {
+        setShowAnswer((prevShowAnswer) => ({...prevShowAnswer, [questionId]: answer}));
+    }
+
+    // Function to handle the "Submit" button click
+    function handleSubmit() {
+        setIsQuizFinished(true);
+    }
 
     // Render the quiz questions
     return (
         <form>
-            {/*{quizData.slice((page - 1) * 10, page * 10).map((question) => {*/}
             {quizData.slice((page - 1), page).map((question) => {
                 return (
                     <div key={question.id}>
